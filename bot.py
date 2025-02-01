14f528f404c52fc575185da74bec308a2857e7c7
Cryptoloverrr99 <digitalmarketingpro22@gmail.com>
2025-02-01 21:49:24 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import requests
 
 response = requests.get(
-    "https://api.dexscreener.com/token-boosts/latest/v1",
+    "https://api.dexscreener.com/latest/dex/pairs?q=all",
     headers={},
 )
 data = response.json()
