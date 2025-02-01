f77f2353d5e8dc539f38dc4344c327039a0e135f
Cryptoloverrr99 <digitalmarketingpro22@gmail.com>
2025-02-01 11:31:05 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+# Configurations API
+
+# Dexscreener
+DEXSCREENER_API_KEY = "ta_cle_dexscreener"  # si nécessaire, sinon laisse vide
+
+# RugChecker
+RUGCHECKER_API_KEY = "ta_cle_rugchecker"
+
+# Bubblemap
+BUBBLEMAP_API_KEY = "ta_cle_bubblemap"
+
+# Twitter
+TWITTER_API_KEY = "ta_cle_twitter"
+TWITTER_API_SECRET = "ton_secret_twitter"
+# ... autres clés selon l’API Twitter que tu utilises
+
+# Telegram Bot
+TELEGRAM_BOT_TOKEN = "ton_token_telegram"
+TELEGRAM_CHAT_ID = "ton_chat_id"  # Identifiant du chat ou du groupe où envoyer les notifications
