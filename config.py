c327587ae81570bb067e27e7c5724c68d2671cb2
Cryptoloverrr99 <digitalmarketingpro22@gmail.com>
2025-02-01 14:07:46 +0000

    Update config.py

--- config.py
+++ config.py
@@ -4,15 +4,7 @@
 DEXSCREENER_API_KEY = "https://api.dexscreener.com/tokens/v1/{chainId}/{tokenAddresses}"  # si nécessaire, sinon laisse vide
 
 # RugChecker
-RUGCHECKER_API_KEY = "rugcheck_api.Risk"
-
-# Bubblemap
-BUBBLEMAP_API_KEY = "https://api.bubblemap.com/check"
-
-# Twitter
-TWITTER_API_KEY = "https://api.twitter.com/2/tweets"
-TWITTER_API_SECRET = "https://api.twitter.com/2/users/search?query=User%20followers%20tweets%20retweets&max_results=1000&next_token=1000&user.fields=most_recent_tweet_id"
-# ... autres clés selon l’API Twitter que tu utilises
+RUGCHECKER_API_KEY = "https://rugcheck_api.Risk"
 
 # Telegram Bot
 TELEGRAM_BOT_TOKEN = "8048350512:AAGVN4uZEt_D1q-ycNN6jhRo-PMn64ZHgiI"
