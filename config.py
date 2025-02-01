52ce92594c31c69789851ec17f9524863ad15c74
Cryptoloverrr99 <digitalmarketingpro22@gmail.com>
2025-02-01 21:48:23 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # Configurations API
 
 # Dexscreener
-DEXSCREENER_API_KEY = "https://api.dexscreener.com/token-boosts/latest/v1"  # si nécessaire, sinon laisse vide
+DEXSCREENER_API_KEY = "https://api.dexscreener.com/latest/dex/pairs?q=all"  # si nécessaire, sinon laisse vide
 
 # RugChecker
-RUGCHECKER_API_KEY = "https://rugcheck_api.Risk"
+RUGCHECKER_API_KEY = "https://api.rugdoc.io/v2/project/{token_address}"
 
 # Telegram Bot
 TELEGRAM_BOT_TOKEN = "8048350512:AAGVN4uZEt_D1q-ycNN6jhRo-PMn64ZHgiI"
