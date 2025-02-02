8894b1df914fa55d9a1cda31f0754ddb07543dbc
Cryptoloverrr99 <digitalmarketingpro22@gmail.com>
2025-02-02 05:02:50 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
-# Configurations API
+# Clés API et configurations importantes
 
-# Dexscreener
-DEXSCREENER_API_KEY = "https://api.dexscreener.com/latest/dex/pairs?q=all"  # si nécessaire, sinon laisse vide
-
-# RugChecker
-RUGCHECKER_API_KEY = "https://api.rugdoc.io/v2/project/{token_address}"
-
-# Telegram Bot
+# Clés API
+SOLSCAN_API_KEY = "votre_cle_api_solscan"
+DEXSCREENER_API_KEY = "votre_cle_api_dexscreener"
 TELEGRAM_BOT_TOKEN = "8048350512:AAGVN4uZEt_D1q-ycNN6jhRo-PMn64ZHgiI"
-TELEGRAM_CHAT_ID = "1002359674981"  # Identifiant du chat ou du groupe où envoyer les notifications
+TELEGRAM_CHAT_ID = "1002359674981"
+
+# URLs des APIs
+DEXSCREENER_API_URL = "https://api.dexscreener.io/latest/tokens"
+SOLSCAN_API_URL = "https://api.solscan.io/account"
+TELEGRAM_API_URL = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
